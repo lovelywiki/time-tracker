b1ae36556b76fb18ad427515bc9dcf29f0c4f814
Ancient Borj <121614275+lovelywiki@users.noreply.github.com>
2023-07-10 19:28:16 +0500

    3 Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,29 +1,5 @@
-import javax.swing.*;
-
-public class TimeTracker {
+public class HelloWorld {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
-
-    private static void createAndShowGUI() {
-        // Create the window
-        JFrame frame = new JFrame("Time-Tracker");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Create a label with the text
-        JLabel label = new JLabel("Time-Tracker");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
-        frame.getContentPane().add(label);
-
-        // Display the window
-        frame.pack();
-        frame.setSize(300, 200);  // Adjust the size as needed
-        frame.setLocationRelativeTo(null); // Center the window
-        frame.setVisible(true);
+        System.out.println("Hello World");
     }
 }
